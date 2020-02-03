360a6b8656dd9ae661c0abe72ced35cc585e1a9f
eunwoo <aufcl4858@naver.com>
2020-02-03 19:07:26 +0900

    예제1

---
+++ big-nerd/src/capter1/Constant.kt
@@ -0,0 +1,9 @@
+package capter1
+
+fun main(args: Array<String>) {
+    val playerName: String = "에스트라곤"
+    var experiencePoints: Int = 5
+    experiencePoints += 5
+    println(experiencePoints)
+    println(playerName)
+}
---
+++ big-nerd/src/capter1/Variable.kt
@@ -0,0 +1,5 @@
+package capter1
+fun main(args:Array<String>) {
+    var experiencePoints: Int = 5
+    println(experiencePoints)
+}
