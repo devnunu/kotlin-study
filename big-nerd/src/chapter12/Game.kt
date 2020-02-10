4023ad6eac26af7edb426c67f7878a446de0402f
eunwoo <aufcl4858@naver.com>
2020-02-10 16:03:56 +0900

    챕터12 완료

---
+++ big-nerd/src/chapter12/Game.kt
@@ -0,0 +1,36 @@
+package chapter12
+
+fun main(args: Array<String>) {
+
+
+    val player = Player()
+    player.castFireball()
+
+    printPlayerStatus(player)
+
+    performCombat()
+    performCombat("Ulrich")
+    performCombat("Hildr", true)
+}
+
+private fun printPlayerStatus(player: Player) {
+    println("(Aura: ${player.auraColor()})" +
+            "(Blessed: ${if (player.isBlessed) "YES" else "NO"})")
+    println("${player.name} ${player.formatHealthStatus()}")
+}
+
+fun performCombat() {
+    println("적군이 없다!")
+}
+
+fun performCombat(enemyName: String) {
+    println("$enemyName 과 전투를 시작함.")
+}
+
+fun performCombat(enemyName: String, isBelessed: Boolean) {
+    if (isBelessed) {
+        println("$enemyName 과 전투를 시작함. 축복을 받음")
+    } else {
+        println("$enemyName 과 전투를 시작함.")
+    }
+}
